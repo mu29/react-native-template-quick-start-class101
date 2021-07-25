9d9b62a082512b09e641392b2a8b6f37d168924e
hero <hero@101.inc>
2021-07-26 02:17:58 +0900

    chore: 의존성 설치 시 기 설치 여부를 확인한다

--- install-deps.sh
+++ install-deps.sh
@@ -3,44 +3,57 @@
 touch ~/.zshrc
 
 # Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ![ -x "$(command -v brew)" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Node.js
-brew install node@14
+if ![ -x "$(command -v node)" ]; then
+  brew install node@14
+  ln -s $(which node) /usr/local/bin/node
 
-ln -s $(which node) /usr/local/bin/node
+  echo 'export PATH="/usr/local/opt/node@14/bin:$PATH"' >> ~/.zshrc
+fi
 
 # Watchman
-brew install watchman
+if ![ -x "$(command -v watchman)" ]; then
+  brew install watchman
+fi
+
+# Java
+if ![ -z brew list -1 | grep -a "adoptopenjdk8"]; then
+  brew install --cask adoptopenjdk/openjdk/adoptopenjdk8
+
+  echo 'export JAVA_HOME=$(/usr/libexec/java_home)' >> ~/.zshrc
+  echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.zshrc
+
+  source ~/.zshrc
+fi
 
 # Android
-brew install --cask adoptopenjdk/openjdk/adoptopenjdk8
+if ![ -z brew list -1 | grep -a "android-sdk"]; then
+  brew install --cask android-sdk
 
-echo 'export PATH="/usr/local/opt/node@14/bin:$PATH"' >> ~/.zshrc
-echo 'export JAVA_HOME=$(/usr/libexec/java_home)' >> ~/.zshrc
-echo 'export PATH=$PATH:$JAVA_HOME/bin' >> ~/.zshrc
+  echo 'export ANDROID_HOME=/usr/local/share/android-sdk' >> ~/.zshrc
+  echo 'export ANDROID_SDK_ROOT=/usr/local/share/android-sdk' >> ~/.zshrc
+  echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.zshrc
+  echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.zshrc
+  echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.zshrc
+  echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.zshrc
 
-source ~/.zshrc
+  source ~/.zshrc
 
-brew install --cask android-sdk
-
-echo 'export ANDROID_HOME=/usr/local/share/android-sdk' >> ~/.zshrc
-echo 'export ANDROID_SDK_ROOT=/usr/local/share/android-sdk' >> ~/.zshrc
-echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.zshrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.zshrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.zshrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.zshrc
-
-source ~/.zshrc
-
-touch ~/.android/repositories.cfg
-sdkmanager "platforms;android-29" "build-tools;29.0.3" "add-ons;addon-google_apis-google-24"
-sdkmanager "system-images;android-29;google_apis_playstore;x86"
-sdkmanager --licenses
-echo "no" | avdmanager --verbose create avd --force --name "test" --package "system-images;android-29;google_apis_playstore;x86" --tag "google_apis_playstore" --abi "x86" --device pixel
+  touch ~/.android/repositories.cfg
+  sdkmanager "platforms;android-29" "build-tools;29.0.3" "add-ons;addon-google_apis-google-24"
+  sdkmanager "system-images;android-29;google_apis_playstore;x86"
+  sdkmanager --licenses
+  echo "no" | avdmanager --verbose create avd --force --name "test" --package "system-images;android-29;google_apis_playstore;x86" --tag "google_apis_playstore" --abi "x86" --device pixel
+fi
 
 # iOS
-brew install cocoapods
+if ![ -x "$(command -v pod)" ]; then
+  brew install cocoapods
+fi
 
 # Additional tools
 python -mwebbrowser https://developer.android.com/studio
